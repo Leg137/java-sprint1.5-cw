00ff05f56f13b5e212ed177395d134d3943cec36
MrOlegB <legan0007@gmail.com>
2025-03-26 14:52:58 +0300

    refactor:

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        new BoboPizza().loop();
+        Scanner scanner = new Scanner(System.in);
+
+        List<BikeTimeCalculator> bikes = new ArrayList<>();
+        bikes.add(new BikeTimeCalculator("Иван"));
+        bikes.add(new BikeTimeCalculator("Василий"));
+        bikes.add(new BikeTimeCalculator("Инга"));
+
+        List<CarTimeCalculator> cars = new ArrayList<>();
+        cars.add(new CarTimeCalculator("Ольга"));
+        cars.add(new CarTimeCalculator("Пётр"));
+
+        List<HumanTimeCalculator> humans = new ArrayList<>();
+        humans.add(new HumanTimeCalculator("Анна"));
+        humans.add(new HumanTimeCalculator("Дмитрий"));
+        humans.add(new HumanTimeCalculator("Алексей"));
+
+        CourierSelector selector = new CourierSelector(bikes, cars, humans);
+
+        DeliveryInfo delivery;
+
+        do {
+            System.out.println("Введите расстояние в метрах => ");
+            int distance = scanner.nextInt();
+            delivery = selector.selectDelivery(distance);
+            if (delivery != null) {
+                System.out.println("Пицца будет доставлена через " + delivery.getTime() + " минут(у,ы)."
+                );
+                System.out.println("Ваш курьер - " + delivery.getName() + ".");
+            } else {
+                System.out.println("Доступных курьеров не найдено");
+            }
+        } while (delivery != null);
     }
 }
